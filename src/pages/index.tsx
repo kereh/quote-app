769b67db9eab8eef556dcf13ccc412fdc9070f6d
kereh <kerehchannel@gmail.com>
2022-09-17 09:30:38 +0800

    get quote from https://api.quotable.io/random and add next button to fetch a new random quote using trpc.useContext() invalidateQuery

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,35 @@
+import { trpc } from "src/utils/trpc"
+
 export default function index() {
+
+    const client = trpc.useContext()
+    const {data:quote} = trpc.useQuery(['quote.getRandomQuote'])
+
     return (
         <div className="container mx-auto p-4">
             <div className="grid place-content-center h-screen">
                 <figure className="p-4 border rounded-lg shadow-lg w-96">
                     <blockquote>
-                        <p className="text-lg font-medium">
-                            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio ab dolores magni doloribus quibusdam repellendus"
+                        {!quote && (
+                            <p className="text-center italic">
+                                Please wait...
+                            </p>
+                        )}
+                        <p className="text-lg font-medium text-gray-700">
+                            {quote?.content && (
+                                <div>
+                                    "{quote.content}"
+                                </div>
+                            )}
                         </p>
                     </blockquote>
-                    <figcaption className="mt-4 italic">
-                        Ronaldo Kereh
-                    </figcaption>
+                    {!quote 
+                        ? "" 
+                        :   <figcaption className="mt-4 flex justify-between">
+                                <span className="italic">{quote?.author}</span> 
+                                <span className="underline cursor-pointer" onClick={() => client.invalidateQueries('quote.getRandomQuote')}>Next</span>
+                            </figcaption>
+                    }
                 </figure>
             </div>
         </div>
