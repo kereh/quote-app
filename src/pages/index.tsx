226d3f4b44a2b568b436d31af6b512a2a68fd232
kereh <kerehchannel@gmail.com>
2022-09-17 08:56:24 +0800

    add index page

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+export default function index() {
+    return (
+        <div className="container mx-auto p-4">
+            <div className="grid place-content-center">
+                <h1 className="text-3xl text-slate-900">Random Quote Generator</h1>
+                <p>Made using Next.js, tRPC and Tailwindcss</p>
+            </div>
+        </div>
+    )
+}
---
+++ src/server/router/quote.ts
@@ -0,0 +1,9 @@
+import { createRouter } from "src/server/router/context"
+
+export const quoteRouter = createRouter()
+    .query('getRandomQuote', {
+        async resolve() {
+            return await fetch('https://api.quotable.io/random')
+                .then((response) => response.json())
+        }
+    })
