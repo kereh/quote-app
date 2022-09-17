d59e6936529a3603532968d585dcd7e4eca37bbc
kereh <kerehchannel@gmail.com>
2022-09-17 09:11:06 +0800

    add quote router to server

--- src/server/router/index.ts
+++ src/server/router/index.ts
@@ -1,12 +1,11 @@
 // src/server/router/index.ts
-import { createRouter } from "./context";
-import superjson from "superjson";
-
-import { exampleRouter } from "./example";
+import { createRouter } from "src/server/router/context"
+import { quoteRouter } from "src/server/router/quote"
+import superjson from "superjson"
 
 export const appRouter = createRouter()
   .transformer(superjson)
-  .merge("example.", exampleRouter);
+  .merge("quote.", quoteRouter);
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
