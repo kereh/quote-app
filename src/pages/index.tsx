ec9a20297e3fab84f9320c55a5e232e7d922160c
kereh <kerehchannel@gmail.com>
2022-09-17 09:08:30 +0800

    add dummy quote

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,17 @@
 export default function index() {
     return (
         <div className="container mx-auto p-4">
-            <div className="grid place-content-center">
-                <h1 className="text-3xl text-slate-900">Random Quote Generator</h1>
-                <p>Made using Next.js, tRPC and Tailwindcss</p>
+            <div className="grid place-content-center h-screen">
+                <figure className="p-4 border rounded-lg shadow-lg w-96">
+                    <blockquote>
+                        <p className="text-lg font-medium">
+                            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio ab dolores magni doloribus quibusdam repellendus"
+                        </p>
+                    </blockquote>
+                    <figcaption className="mt-4 italic">
+                        Ronaldo Kereh
+                    </figcaption>
+                </figure>
             </div>
         </div>
     )
